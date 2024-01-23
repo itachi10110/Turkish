a04ea4b2991c2e06f56f30dad87dfc978cdd1561
itachi10110 <130030034+itachi10110@users.noreply.github.com>
2024-01-23 18:12:56 +0100

    Add files via upload

---
+++ modern-portfolio-main/modern-portfolio-main/components/Avatar.jsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+const Avatar = () => {
+  return (
+    <div className="hidden xl:flex xl:max-w-none pointer-events-none select-none">
+      <Image
+        src="/avatar.png"
+        alt="avatar"
+        className="translate-z-0 w-full h-full"
+      />
+    </div>
+  );
+};
+
+export default Avatar;
---
+++ modern-portfolio-main/modern-portfolio-main/components/TestimonialSlider.jsx
@@ -0,0 +1,92 @@
+import Image from "next/image";
+import { FaQuoteLeft } from "react-icons/fa";
+import { Navigation, Pagination } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+const testimonialData = [
+  {
+    image: "/t-avt-1.png",
+    name: "Jose N. Harris",
+    position: "",
+    message:
+      "To get something you never had, you have to do something you never did , The will of God will never take you where the Grace of God will not protect you.",
+  },
+  {
+    image: "/t-avt-2.png",
+    name: "Aristotle",
+    position: "",
+    message:
+      "I count him braver who overcomes his desires than him who conquers his enemies, for the hardest victory is over self.",
+  },
+  {
+    image: "/t-avt-3.png",
+    name: "Vladimir Putin",
+    position: "",
+    message:
+      "It's better to be hanged for loyalty than rewarded for betrayal",
+  },
+];
+
+const TestimonialSlider = () => {
+  return (
+    <Swiper
+      navigation
+      pagination={{
+        clickable: true,
+      }}
+      modules={[Navigation, Pagination]}
+      className="h-[400px]"
+    >
+      {testimonialData.map((person, i) => (
+        <SwiperSlide key={i}>
+          <div className="flex flex-col items-center md:flex-row gap-x-8 h-full px-16">
+            {/* avatar, name, position */}
+            <div className="w-full max-w-[300px] flex flex-col xl:justify-center items-center relative mx-auto xl:mx-0">
+              <div className="flex flex-col justify-center text-center">
+                {/* avatar */}
+                <div className="mb-2 mx-auto">
+                  <Image
+                    src={person.image}
+                    width={100}
+                    height={100}
+                    alt={person.name}
+                  />
+                </div>
+
+                {/* name */}
+                <div className="text-lg">{person.name}</div>
+
+                {/* position */}
+                <div className="text-[12px] uppercase font-extralight tracking-widest">
+                  {person.position}
+                </div>
+              </div>
+            </div>
+
+            {/* quote & message */}
+            <div className="flex-1 flex flex-col justify-center before:w-[1px] xl:before:bg-white/20 xl:before:absolute xl:before:left-0 xl:before:h-[200px] relative xl:pl-20">
+              {/* quote icon */}
+              <div className="mb-4">
+                <FaQuoteLeft
+                  className="text-4xl xl:text-6xl text-white/20 mx-auto md:mx-0"
+                  aria-aria-hidden
+                />
+              </div>
+
+              {/* message */}
+              <div className="xl:text-lg text-center md:text-left">
+                {person.message}
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
+
+export default TestimonialSlider;
